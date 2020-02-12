82470ba3b8058f3d688ca8a6f08fa21941008e4a
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-02-12 11:58:26 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,21 @@
+<?php
+$domOBJ = new DOMDocument();
+$domOBJ->load("http://scanvenger.herokuapp.com/");
+
+$content = $domOBJ->getElementsByTagName("item");
+
+foreach ($content as $data) {
+
+    $name = $data->getElementsByTagName("name")->item(0)->nodeValue;
+    $gende = $data->getElementsByTagName("gender")->item(0)->nodeValue;
+    $age = $data->getElementsByTagName("age")->item(0)->nodeValue;
+    $fav = $data->getElementsByTagName("favorite No")->item(0)->nodeValue;
+
+    echo "
+    <ul>
+    <li>name: <strong>$name</strong></li>
+    <li>gender: <strong>$gender</strong></li>
+    <li>age: <strong>$age</strong></li>
+    <li>fav: <strong>$fav</strong></li>
+    </ul>";
+}
